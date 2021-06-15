42e2b900f908457c791ced4a7480fcfebc762f87
lucasdemicco <lucasmicco@hotmail.com>
2021-06-15 10:37:41 -0300

    Add slides

--- app/src/main/java/View/BemVindoActivity.java
+++ app/src/main/java/View/BemVindoActivity.java
@@ -4,13 +4,33 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 import com.lucas.adocaoapp.R;
 
-public class BemVindoActivity extends AppCompatActivity {
+public class BemVindoActivity extends IntroActivity {
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bem_vindo);
-    }
+        //setContentView(R.layout.activity_bem_vindo);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.black)
+                .fragment(R.layout.intro1)
+                .canGoBackward(false)
+                .build());
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_orange_dark)
+                .fragment(R.layout.intro2)
+                .canGoForward(false)
+                .build());
+        }
 }
