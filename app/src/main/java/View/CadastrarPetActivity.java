c83bc4e6644c0abe51a71e1d858c52a9a37a50e6
lucasdemicco <lucasmicco@hotmail.com>
2021-06-23 11:21:30 -0300

    "Add permissions for uses camera and galery user"

--- app/src/main/java/View/CadastrarPetActivity.java
+++ app/src/main/java/View/CadastrarPetActivity.java
@@ -1,16 +1,64 @@
 package View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 
 import com.lucas.adocaoapp.R;
 
+import org.jetbrains.annotations.NotNull;
+
+import Controller.Permissoes;
+
 public class CadastrarPetActivity extends AppCompatActivity {
+
+    private String [] permissoes = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_pet);
+
+        //Validar permissões
+        Permissoes.validarPermissoes(permissoes, this, 1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for (int permissaoResultado : grantResults) {
+            if (permissaoResultado == PackageManager.PERMISSION_DENIED) {
+                alertaValidacaoPermissao();
+            }
+        }
+    }
+
+    private void alertaValidacaoPermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões negadas");
+        builder.setMessage("Para utilizar a câmera ou galeria é necessário aceitar as permissões");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void cadastrarPet(View view) {
     }
 }
